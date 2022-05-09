3cea9d2c5399c923cca72ee9a1ea644294f58b74
folaketee <folaketee@gmail.com>
2022-05-09 19:57:25 +0100

    advice generator - remove window.reload

--- src/App.js
+++ src/App.js
@@ -1,35 +1,30 @@
 import './App.scss';
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 
 const App = () => {
     const [data, setData] = useState(null);
     const [isLoading, setLoading] = useState(true);
-    const [toggle, setToggle] = useState(true);
 
     const baseUrl = 'https://api.adviceslip.com/advice';
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.get(baseUrl)
-                setData(response.data);
-                setLoading(false);
-            } catch (err) {
-                if (err.response) {
-                    console.log(err.response)
-                } else {
-                    console.log(`Error: ${err.message}`);
-                }
+    const fetchData = useCallback(async () => {
+        try {
+            const response = await axios.get(baseUrl)
+            setData(response.data);
+            setLoading(false);
+        } catch (err) {
+            if (err.response) {
+                console.log(err.response)
+            } else {
+                console.log(`Error: ${err.message}`);
             }
         }
+    }, [ setData, setLoading ]);
 
+    useEffect(() => {
         fetchData();
-    }, [toggle]);
-
-    const refresh = () => {
-        setToggle(!toggle);
-    }
+    }, [fetchData]);
 
   return (
     <main className='container'>
@@ -41,7 +36,7 @@ const App = () => {
               </>
           }
           <img src='../images/pattern-divider-desktop.svg' alt='pattern divider' className='pattern-divider'/>
-          <div className='icon-dice' onClick={refresh}>
+          <div className='icon-dice' onClick={fetchData}>
               <img src='../images/icon-dice.svg' alt='icon dice'/>
           </div>
       </div>
