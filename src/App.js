16b7acff1bdfbfec82b8cdc22bfe7fd9e6aff432
folaketee <folaketee@gmail.com>
2022-05-09 15:33:06 +0100

    advice generator - add loading state

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import React, { useEffect, useState } from 'react';
 
 const App = () => {
     const [data, setData] = useState(null);
+    const [isLoading, setLoading] = useState(true);
+
     const baseUrl = 'https://api.adviceslip.com/advice';
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const response = await axios.get(baseUrl)
-                setData(response.data)
+                setData(response.data);
+                setLoading(false);
             } catch (err) {
                 if (err.response) {
                     console.log(err.response)
@@ -31,8 +34,12 @@ const App = () => {
   return (
     <main className='container'>
       <div className='content'>
-          <h1 className='content__heading'>Advice #{data && data.slip.id}</h1>
-          <p className='content__quotes'>"{data && data.slip.advice}"</p>
+          {isLoading ? <p className='loading'>"Loading...."</p> :
+              <>
+                  <h1 className='content__heading'>Advice #{data && data.slip.id}</h1>
+                  <p className='content__quotes'>"{data && data.slip.advice}"</p>
+              </>
+          }
           <img src='../images/pattern-divider-desktop.svg' alt='pattern divider' className='pattern-divider'/>
           <div className='icon-dice' onClick={generateNewAdvice}>
               <img src='../images/icon-dice.svg' alt='icon dice'/>
